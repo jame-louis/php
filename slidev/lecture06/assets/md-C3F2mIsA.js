1f43f76dff1c6646621a94dbb67a257ad22f139f
jamelouis <jamelouis2015@gmail.com>
2026-04-09 21:56:34 +0800

    add lecture06 - custom function, string function, image function

---
+++ slidev/lecture06/assets/md-C3F2mIsA.js
@@ -0,0 +1,14 @@
+import{_ as k}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-LHuYBisN.js";import{o as p,b as h,w as e,g as s,d as o,m as d,ad as i,v as c,x as g,T as l}from"./modules/vue-ay11y6SF.js";import{I as m}from"./slidev/default-CRLCSMm5.js";import{u as f,f as u}from"./slidev/context-BG8WSvBj.js";import"./modules/unplugin-icons-Cxzm2Ki6.js";import"./index-T0UcKxcj.js";import"./modules/shiki-D6djOZTy.js";const T={__name:"slides.md__slidev_38",setup(A){const{$clicksContext:t,$frontmatter:n}=f();return t.setup(),(y,a)=>{const r=k;return p(),h(m,c(g(l(u)(l(n),37))),{default:e(()=>[a[1]||(a[1]=s("h2",null,"动手练 5.1",-1)),a[2]||(a[2]=s("p",null,"使用字符串函数完成任务：",-1)),o(r,d({lines:!0,maxHeight:"400px"},{title:"",ranges:["*"]}),{default:e(()=>[...a[0]||(a[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-php"},[s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#CB7676","--shiki-light":"#AB5959"}},"<?"),s("span",{style:{"--shiki-dark":"#C99076","--shiki-light":"#A65E2B"}},"php")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}},"// 动手练 5.1：使用字符串函数完成以下任务")]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},"$"),s("span",{style:{"--shiki-dark":"#BD976A","--shiki-light":"#B07D48"}},"str"),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}}," ="),s("span",{style:{"--shiki-dark":"#C98A7D77","--shiki-light":"#B5695977"}},' "'),s("span",{style:{"--shiki-dark":"#C98A7D","--shiki-light":"#B56959"}},"The quick brown fox jumps over the lazy dog"),s("span",{style:{"--shiki-dark":"#C98A7D77","--shiki-light":"#B5695977"}},'"'),s("span",{style:{"--shiki-dark":"#666666","--shiki-light":"#999999"}},";")]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}},"// 任务1：将 fox 替换为 cat")]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}},"// 任务2：提取前9个字符（The quick）")]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}},"// 任务3：查找 dog 的位置")]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#CB7676","--shiki-light":"#AB5959"}},"?>")])])],-1)])]),_:1},16)]),_:1},16)}}};export{T as default};
