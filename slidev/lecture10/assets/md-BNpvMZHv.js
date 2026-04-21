51f9adc9aba770a70a65bb2c2eec82e0f70af5ee
jamelouis <jamelouis2015@gmail.com>
2026-04-21 14:38:09 +0800

    add lecture 10

---
+++ slidev/lecture10/assets/md-BNpvMZHv.js
@@ -0,0 +1,23 @@
+import{_ as p}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-CvQE9fy8.js";import{o as h,b as r,w as n,g as s,d as D,m as A,ad as i,v as o,x as d,T as l}from"./modules/vue-HrgJh-zP.js";import{I as c}from"./slidev/default-DFhSs2w1.js";import{u as g,f as m}from"./slidev/context-VT7WgeB7.js";import"./modules/unplugin-icons-LxEQKk-c.js";import"./index-BXO997rG.js";import"./modules/shiki-DVlQnERR.js";const v={__name:"slides.md__slidev_29",setup(y){const{$clicksContext:e,$frontmatter:t}=g();return e.setup(),(f,a)=>{const k=p;return h(),r(c,o(d(l(m)(l(t),28))),{default:n(()=>[a[1]||(a[1]=s("h3",null,"挑战：处理登录",-1)),D(k,A({lines:!0,maxHeight:"320px"},{title:"",ranges:["*"]}),{default:n(()=>[...a[0]||(a[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-php"},[s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#CB7676","--shiki-light":"#AB5959"}},"<?"),s("span",{style:{"--shiki-dark":"#C99076","--shiki-light":"#A65E2B"}},"php")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}},"/**")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}}," * 挑战：记住我登录系统 - 处理登录")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}}," *")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}}," * 需求：")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}}," * 1. 接收用户名和密码")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}}," * 2. 验证用户名密码（模拟：admin/123456）")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}},' * 3. 如果"记住我"被勾选，设置7天有效期的Cookie')]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}}," * 4. 否则设置会话Cookie")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}}," * 5. 登录成功后跳转到 ex-challenge-index.php")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}}," */")]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}},"// TODO: 获取POST数据")]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}},'// TODO: 检查是否勾选"记住我"')]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}},"// TODO: 验证用户名和密码")]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}},'// TODO: 设置Cookie（根据"记住我"决定过期时间）')]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}},"// TODO: 跳转或显示错误")]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#CB7676","--shiki-light":"#AB5959"}},"?>")])])],-1)])]),_:1},16)]),_:1},16)}}};export{v as default};
---
+++ slidev/lecture10/assets/md-CSa--WjH.js
@@ -0,0 +1,14 @@
+import{_ as r}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-CvQE9fy8.js";import{o as k,b as h,w as e,g as s,d as o,m as d,ad as i,v as c,x as A,T as l}from"./modules/vue-HrgJh-zP.js";import{I as D}from"./slidev/default-DFhSs2w1.js";import{u as g,f as m}from"./slidev/context-VT7WgeB7.js";import"./modules/unplugin-icons-LxEQKk-c.js";import"./index-BXO997rG.js";import"./modules/shiki-DVlQnERR.js";const b={__name:"slides.md__slidev_31",setup(f){const{$clicksContext:n,$frontmatter:t}=g();return n.setup(),(u,a)=>{const p=r;return k(),h(D,c(A(l(m)(l(t),30))),{default:e(()=>[a[1]||(a[1]=s("h3",null,"挑战：登出",-1)),o(p,d({lines:!0,maxHeight:"280px"},{title:"",ranges:["*"]}),{default:e(()=>[...a[0]||(a[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-php"},[s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#CB7676","--shiki-light":"#AB5959"}},"<?"),s("span",{style:{"--shiki-dark":"#C99076","--shiki-light":"#A65E2B"}},"php")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}},"/**")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}}," * 挑战：记住我登录系统 - 登出")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}}," *")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}}," * 需求：")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}}," * 1. 删除所有与用户登录相关的Cookie")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}}," * 2. 跳转到 ex-challenge-login.php")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}}," */")]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}},"// TODO: 删除用户相关的Cookie")]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#758575DD","--shiki-light":"#A0ADA0"}},"// TODO: 跳转到登录页面")]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#CB7676","--shiki-light":"#AB5959"}},"?>")])])],-1)])]),_:1},16)]),_:1},16)}}};export{b as default};
